ec5c9d5a8f22c8f53f68b8cf9f31efad5446ee83
Penny Huang <pennyhuang@synology.com>
2023-03-27 17:12:08 +0800

    Move FeedLoaderWithFallbackComposite to production

--- FeedAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ FeedAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -7,36 +7,13 @@
 
 import XCTest
 import Feed
-
-class FeedLoaderWithFallbackComposite: FeedLoader {
-    private let primary: FeedLoader
-    private let fallback: FeedLoader
-    
-    init(primary: FeedLoader, fallback: FeedLoader) {
-        self.primary = primary
-        self.fallback = fallback
-    }
-
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        primary.load { [weak self] result in
-                switch result {
-                case .success:
-                    completion(result)
-
-                case .failure:
-                    self?.fallback.load(completion: completion)
-                }
-            }
-    }
-}
+import FeedApp
 
 class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 
     func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
         let primaryFeed = uniqueFeed()
         let fallbackFeed = uniqueFeed()
-        let primaryLoader = LoaderStub(result: .success(primaryFeed))
-        let fallbackLoader = LoaderStub(result: .success(fallbackFeed))
         let sut = makeSUT(primaryResult: .success(primaryFeed), fallbackResult: .success(fallbackFeed))
 
         expect(sut, toCompleteWith: .success(primaryFeed))
