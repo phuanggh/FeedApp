6c2f0dad3672229324673a8702e97843a7133acd
Penny Huang <pennyhuang@synology.com>
2023-03-30 15:03:45 +0800

    Extract FeedImageDataLoader test helpers into a shared protocol extension to remove duplication

---
+++ FeedAppTests/Helpers/XCTestCase+FeedImageDataLoader.swift
@@ -0,0 +1,36 @@
+//
+//  XCTestCase+FeedImageDataLoader.swift
+//  FeedAppTests
+//
+//  Created by Penny Huang on 2023/3/30.
+//
+
+import XCTest
+import Feed
+
+protocol FeedImageDataLoaderTestCase: XCTestCase {}
+
+extension FeedImageDataLoaderTestCase {
+    func expect(_ sut: FeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+
+        _ = sut.loadImageData(from: anyURL()) { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedFeed), .success(expectedFeed)):
+                XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
+
+            case (.failure, .failure):
+                break
+
+            default:
+                XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+
+            exp.fulfill()
+        }
+
+        action()
+
+        wait(for: [exp], timeout: 1.0)
+    }
+}
