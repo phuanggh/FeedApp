cab40c6ed958f068f5c054213469d78c61aa3915
Penny Huang <pennyhuang@synology.com>
2023-03-30 15:16:26 +0800

    FeedImageDataLoaderCacheDecorator.loadImageData does not cache image data on loader failure

--- FeedAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
+++ FeedAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -26,8 +26,10 @@ class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
 
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
         return decoratee.loadImageData(from: url) { [weak self] result in
-            self?.cache.save((try? result.get()) ?? Data(), for: url) { _ in }
-            completion(result)
+            completion(result.map { data in
+                self?.cache.save((try? result.get()) ?? Data(), for: url) { _ in }
+                return data
+            })
         }
     }
 }
@@ -87,6 +89,17 @@ class FeedImageDataLoaderCacheDecoratorTests: XCTestCase, FeedImageDataLoaderTes
 
         XCTAssertEqual(cache.messages, [.save(data: imageData, for: url)], "Expected to cache loaded image data on success")
     }
+    
+    func test_loadImageData_doesNotCacheDataOnLoaderFailure() {
+        let cache = CacheSpy()
+        let url = anyURL()
+        let (sut, loader) = makeSUT(cache: cache)
+
+        _ = sut.loadImageData(from: url) { _ in }
+        loader.complete(with: anyNSError())
+
+        XCTAssertTrue(cache.messages.isEmpty, "Expected not to cache image data on load error")
+    }
 
     // MARK: - Helpers
         
@@ -104,7 +117,6 @@ class FeedImageDataLoaderCacheDecoratorTests: XCTestCase, FeedImageDataLoaderTes
         enum Message: Equatable {
             case save(data: Data, for: URL)
         }
-
 
         func save(_ data: Data, for url: URL, completion: @escaping (FeedImageDataCache.Result) -> Void) {
             messages.append(.save(data: data, for: url))
