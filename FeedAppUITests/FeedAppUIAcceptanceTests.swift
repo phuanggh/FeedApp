4444dab5f0e4d0f264086a7e2d6b1f493fce0221
Penny Huang <pennyhuang@synology.com>
2023-04-07 16:47:55 +0800

    improve coverage by using identifiers for finding feed image cells

--- FeedAppUITests/FeedAppUIAcceptanceTests.swift
+++ FeedAppUITests/FeedAppUIAcceptanceTests.swift
@@ -11,12 +11,24 @@ import FeediOS
 
 final class FeedAppUIAcceptanceTests: XCTestCase {
 
+//    func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
+//        let app = XCUIApplication()
+//
+//        app.launch()
+//
+//        XCTAssertEqual(app.cells.count, 22)
+//        XCTAssertEqual(app.cells.firstMatch.images.count, 1)
+//    }
+//
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let app = XCUIApplication()
         
         app.launch()
         
-        XCTAssertEqual(app.cells.count, 22)
-        XCTAssertEqual(app.cells.firstMatch.images.count, 1)
+        let feedCells = app.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(feedCells.count, 22)
+        
+        let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
+        XCTAssertTrue(firstImage.exists)
     }
 }
