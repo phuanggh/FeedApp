b778ac65c23e36d31897bdcc401756779b4186ab
Penny Huang <pennyhuang@synology.com>
2023-04-14 10:52:35 +0800

    Fix potential bug when cancelling requests in UITableView didEndDisplayingCell method - This method is invoked after calling reloadData, so we'd be cancelling requests in the wrong models or crash in case the model has less items than the previous model

--- FeedAppTests/FeedUIIntegrationTests.swift
+++ FeedAppTests/FeedUIIntegrationTests.swift
@@ -83,6 +83,20 @@ final class FeedUIIntegrationTests: XCTestCase {
         assertThat(sut, isRendering: [image0, image1, image2, image3])
     }
     
+    func test_loadFeedCompletion_rendersSuccessfullyLoadedEmptyFeedAfterNonEmptyFeed() {
+        let image0 = makeImage()
+        let image1 = makeImage()
+        
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading(with: [image0, image1], at: 0)
+        assertThat(sut, isRendering: [image0, image1])
+        
+        sut.simulateUserInitiatedFeedReload()
+        loader.completeFeedLoading(with: [], at: 1)
+        assertThat(sut, isRendering: [])
+    }
+    
     func test_loadFeedCompletion_doesNotAlterCurrentRenderingStateOnError() {
         let image0 = makeImage()
         let (sut, loader) = makeSUT()
@@ -324,6 +338,12 @@ final class FeedUIIntegrationTests: XCTestCase {
     }
     
     private func assertThat(_ sut: FeedViewController, isRendering feed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
+        // tableView.reloadData does not force an immediate layout update.
+        // didEndDisplayingCell will only be called in the next layout cycle
+        // so, to force table view to layout:
+        sut.tableView.layoutIfNeeded()
+        RunLoop.main.run(until: Date())
+        
         guard sut.numberOfRenderedFeedImageViews() == feed.count else {
             return XCTFail("Expected \(feed.count) image, got \(sut.numberOfRenderedFeedImageViews()) instead", file: file, line: line)
         }
